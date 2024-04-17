4c5873a47a149dd4a2846709e57848bde296f772
saiful islam <saifulbrur79@gmail.com>
2024-04-17 15:03:23 +0600

    update setup file

--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
-# import setuptools
 
-# with open("README.md", "r", encoding = "utf-8") as fh:
-#     long_description = fh.read()
-
-# setuptools.setup(
-#     name = "pybangla",
-#     version = "0.0.1",
-#     author = "saiful",
-#     author_email = "saifulbrur79@gmail.com",
-#     description = "format date to bangla date format and english digits convert to bangla digits",
-#     long_description = long_description,
-#     long_description_content_type = "text/markdown",
-#     url = "package URL",
-#     project_urls = {
-#         "Bug Tracker": "package issues URL",
-#     },
-#     classifiers = [
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
-#     install_requires = {"DateTime"},
-#     package_dir = {"": "src"},
-#     packages = setuptools.find_packages(where="src"),
-#     python_requires = ">=3.6"
-# )
-
+import codecs
 from setuptools import setup, find_packages
 
 setup(
-    name='example-package',
-    version='0.1',
+    name='pybangla',
+    version='0.0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -39,10 +13,18 @@ setup(
     },
     author='saiful',
     author_email='saifulbrur79@gmail.com',
-    description='An example package',
+    description='date and number digits convert to bangla digits',
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/saiful9379/pybangla',
     license='MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
-        # List of dependencies
-    ]
+        "DateTime"
+    ],
+    python_requires = ">=3.6"
 )
