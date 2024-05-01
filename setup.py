2223320bada90cb01f5a50cabb8f1908fa035212
saiful islam <saifulbrur79@gmail.com>
2024-05-01 15:38:25 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     # entry_points={
     #     'console_scripts': [
