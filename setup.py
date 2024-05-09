fc4e3bc058ea310e3ed560a72e5f5305f7ede576
saiful <saifulbrur79@mail.com>
2024-05-09 15:53:49 +0600

    solved expanding issue

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     # entry_points={
     #     'console_scripts': [
