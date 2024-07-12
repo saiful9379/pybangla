323fa89c890adf64bd5e140a669a15fd070bb5ae
saiful <saifulbrur79@mail.com>
2024-07-12 23:25:43 +0600

    solved english numerical issue for year format

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(),
     # entry_points={
     #     'console_scripts': [
