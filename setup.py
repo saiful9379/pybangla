66b8f6265078d660a2f5dffbe88bbdee6f526838
saiful9379 <saifulbrur79@gmail.com>
2025-05-02 03:09:02 +0600

    update number pronouncition

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='2.0.9',
+    version='2.10.0',
     packages=find_packages(),
     # entry_points={
     #     'console_scripts': [
