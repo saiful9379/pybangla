76282dee668c3dbee785e59a4315e1aa587ba7d3
saiful9379 <saifulbrur79@gmail.com>
2025-09-28 16:30:08 +0600

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='2.11.9',
+    version='2.12.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
