1664f23dcadf334236821575430a3148be615af3
saiful9379 <saifulbrur79@gmail.com>
2025-11-13 21:36:02 +0600

    add new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybangla',
-    version='2.18.0',
+    version='2.19.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
